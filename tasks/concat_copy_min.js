b31d5c65ee729703cc5fdfeae41ca9519414e3d0
Hugin <hxjlucky@gmail.com>
2015-05-25 11:11:47 +0800

    gulp-shell to run r.js

--- tasks/concat_copy_min.js
+++ tasks/concat_copy_min.js
@@ -5,29 +5,31 @@ var path = require('path');
 var concat = require('gulp-concat');
 var rjs = require('gulp-requirejs');
 var minifyCSS = require('gulp-minify-css');
+var shell = require('gulp-shell');
+var rjsa = require('requirejs');
 
 // start copy
 gulp.task('copy', function() {
-  return gulp.src([
-    // './static/bower_components/teambition-ui/dist/fonts/*',
-    // './static/images/**'
-  ], {
-    base: path.join(__dirname, '../static')
+    return gulp.src([
+        // './static/bower_components/teambition-ui/dist/fonts/*',
+        // './static/images/**'
+      ], {
+        base: path.join(__dirname, '../static')
+      })
+      .pipe(gulp.dest('./dist'));
   })
-    .pipe(gulp.dest('./dist'));
-})
-// end copy
+  // end copy
 
 
 
 // start min script
 gulp.task('uglify', function() {
   return gulp.src([
-    // './static/js/base.js',
-    // './static/js/common.js'
-  ], {
-    base: path.join(__dirname, '../static/')
-  })
+      // './static/js/base.js',
+      // './static/js/common.js'
+    ], {
+      base: path.join(__dirname, '../static/')
+    })
     .pipe(uglify())
     .pipe(gulp.dest('./dist/js/'));
 });
@@ -63,32 +65,85 @@ gulp.task('jscommon', function() {
 // end concat js
 
 // start min style
-gulp.task('cssmin', function () {
-  return gulp.src([
-    // './static/bower_components/teambition-ui/dist/css/teambition-ui.css',
-    './static/css/lib/jquery/jquery.css'
-  ], {
-    base: path.join(__dirname, '../static')
+gulp.task('cssmin', function() {
+    return gulp.src([
+        // './static/bower_components/teambition-ui/dist/css/teambition-ui.css',
+        './static/css/lib/jquery/jquery.css'
+      ], {
+        base: path.join(__dirname, '../static')
+      })
+      .pipe(minifyCSS())
+      .pipe(gulp.dest('./dist'))
   })
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('./dist'))
-})
-// end min style
+  // end min style
 
 
 // start requirejs
 gulp.task('requirejs', function() {
   return rjs({
-    name: 'main',
-    baseUrl: 'static/js',
-    mainConfigFile: 'static/js/main.js',
-    out: 'main.js',
-    shim: {},
-  })
+      name: 'main',
+      baseUrl: 'static/js',
+      mainConfigFile: 'static/js/main.js',
+      out: 'main.js',
+      exclude: ["backbone",
+        "jquery",
+        "swiper",
+        "underscore"
+      ],
+      shim: {},
+    })
     // .pipe(uglify())
     .pipe(gulp.dest('./dist/js/'));
 });
 // end requirejs
 
+
+// scripts start, use gulp-shell to run r.js
+gulp.task('scripts', shell.task([
+  'r.js -o build.js'
+]));
+// scripts end
+
+
+// build start
+gulp.task('build', function(cb) {
+  rjsa.optimize({
+    // appDir: 'www',
+    baseUrl: '../static/js',
+    paths: {
+      machina: "empty:"
+    },
+    dir: 'dist',
+    modules: [{
+      name: "base"
+    }, {
+      name: "main-a",
+      include: ["page/a.js"],
+      exclude: [
+        "base"
+      ]
+    }, {
+      name: "main-b",
+      include: ["page/b.js"],
+      exclude: [
+        "base"
+      ]
+    }, {
+      name: "h5-base"
+    }, {
+      name: "h5-main-a",
+      include: ["page/h5-a.js"],
+      exclude: [
+        "h5-base"
+      ]
+    }]
+  }, function(buildResponse) {
+    // console.log('build response', buildResponse);
+    cb();
+  }, cb);
+});
+// build end
+
+
 // gulp.task('min', ['copy', 'cssmin', 'requirejs', 'uglify'])
-gulp.task('min', ['cssmin', 'jsbase', 'jscommon','requirejs'])
+gulp.task('min', ['cssmin', 'jsbase', 'jscommon', 'requirejs'])
