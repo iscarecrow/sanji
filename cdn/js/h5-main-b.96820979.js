6e70e3ce7b479e1efffbbf3b96cfc5b6fbb9f7b3
Hugin <hxjlucky@gmail.com>
2015-06-14 00:07:48 +0800

    delete cdn

--- cdn/js/h5-main-b.96820979.js
+++
@@ -1,4 +0,0 @@
-requirejs( [ "h5-base.92b85dae" ], function () {
-  requirejs( [ "page/h5-b.fb651c9e.js" ], function (a) {
-  });
-});
--- cdn/js/page/h5-b.fb651c9e.js
+++
@@ -1,4 +0,0 @@
-define(['zepto'], function($) {
-  console.log('page-h5-b.js');
-  // $('div').anim({ rotate: '720deg', opacity: .5 }, 2, 'ease-out');
-});
